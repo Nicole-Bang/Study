e41d48bf871fb2df5fbe856a4ca25641f63aca06
Nicole_B <110227669+Nicole-Bang@users.noreply.github.com>
2023-01-02 11:55:19 +0900

    Delete project02 directory

--- project02/src/views/AboutView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h2>ITWILL 서울 강남구 역삼동</h2>
-  </div>
-</template>
--- project02/src/views/WelcomeView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-      <h1>Welcome To Myweb</h1>
-      <h2>환영합니다</h2>
-    </div>
-</template>
